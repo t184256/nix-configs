0fcf531a15bb24b7c0f9ef19a3d362cf6823c948
Alexander Sosedkin <monk@unboiled.info>
2020-11-08 17:42:19 +0000

    user/fonts: add explicit dependencies

--- user/fonts.nix
+++ user/fonts.nix
@@ -1,33 +1,42 @@
 { pkgs, lib, config, ... }:
 
-lib.mkIf (! config.system.noGraphics) {
-  home.packages = with pkgs; [
-    iosevka-t184256                     # overlay: iosevka-t184256
-    google-fonts.just.RobotoCondensed   # overlay: select-google-fonts
-    noto-fonts-extracondensed           # overlay: noto-fonts-extracondensed
+{
+  imports = [ ./config/no-graphics.nix ];
+  nixpkgs.overlays = [
+   (import ../overlays/iosevka-t184256.nix)
+   (import ../overlays/select-google-fonts.nix)
+   (import ../overlays/noto-fonts-extracondensed.nix)
   ];
-  home.file.".config/fontconfig/fonts.conf".text = ''
-    <?xml version="1.0"?>
-    <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-    <fontconfig>
 
-      <!-- Only works OK with firefox if there's no regular Noto -->
+  home = if config.system.noGraphics then {} else {
+   packages = with pkgs; [
+     iosevka-t184256                     # overlay: iosevka-t184256
+     google-fonts.just.RobotoCondensed   # overlay: select-google-fonts
+     noto-fonts-extracondensed           # overlay: noto-fonts-extracondensed
+   ];
+   file.".config/fontconfig/fonts.conf".text = ''
+     <?xml version="1.0"?>
+     <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+     <fontconfig>
 
-      <alias>
-        <family>sans-serif</family>
-        <prefer><family>Noto Sans ExtraCondensed</family></prefer>
-      </alias>
+       <!-- Only works OK with firefox if there's no regular Noto -->
 
-      <alias>
-        <family>serif</family>
-        <prefer><family>Noto Serif ExtraCondensed</family></prefer>
-      </alias>
+       <alias>
+         <family>sans-serif</family>
+         <prefer><family>Noto Sans ExtraCondensed</family></prefer>
+       </alias>
 
-      <alias>
-        <family>monospace</family>
-        <prefer><family>Iosevka Term</family></prefer>
-      </alias>
+       <alias>
+         <family>serif</family>
+         <prefer><family>Noto Serif ExtraCondensed</family></prefer>
+       </alias>
 
-    </fontconfig>
-  '';
+       <alias>
+         <family>monospace</family>
+         <prefer><family>Iosevka Term</family></prefer>
+       </alias>
+
+     </fontconfig>
+   '';
+  };
 }
