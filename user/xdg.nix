7956e8040a11b5c546c75682c58897b353776757
Alexander Sosedkin <monk@unboiled.info>
2020-11-12 04:17:15 +0100

    user/xdg: configure user directories

---
+++ user/xdg.nix
@@ -0,0 +1,15 @@
+{ ... }:
+
+{
+  xdg.userDirs = {
+    enable = true;
+    desktop = "$HOME/.desktop";
+    documents = "$HOME/documents";
+    download = "$HOME/.downloads";
+    music = "$HOME/music";
+    pictures = "$HOME/pictures";
+    publicShare = "$HOME/.public";
+    templates = "$HOME/documents/templates";
+    videos = "$HOME/video";
+  };
+}
