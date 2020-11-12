4674197114223186758f538cd95b4225cf06b893
Alexander Sosedkin <monk@unboiled.info>
2020-11-12 01:32:43 +0100

    user/language-support: add C

--- user/config/language-support.nix
+++ user/config/language-support.nix
@@ -3,7 +3,17 @@
 {
   options.language-support = lib.mkOption {
     default = [];
-    type = lib.types.listOf (lib.types.enum [ "nix" "python" "bash" "tex" ]);
-    description = "Which languages to install tools for";
+    type = lib.types.listOf (lib.types.enum [
+      "nix"
+      "python"
+      "bash"
+      "tex"
+      "c"
+    ]);
+    description = ''
+      Which languages to install additional tools for.
+      For programming languages, don't expect tools available on your $PATH,
+      this is only about the text editor support.
+    '';
   };
 }
