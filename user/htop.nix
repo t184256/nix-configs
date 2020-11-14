7d0ac04bd439d6d2a7a28b699dbbe0ae99a790f7
Alexander Sosedkin <monk@unboiled.info>
2020-11-14 03:16:29 +0000

    user/htop: tweak for Nix-on-Droid

--- user/htop.nix
+++ user/htop.nix
@@ -1,16 +1,28 @@
 { config, pkgs, ... }:
 
 {
+  imports = [ ./config/os.nix ];
   programs.htop = {
     enable = true;
     colorScheme = 1;
-    fields = [ "USER" "PERCENT_CPU" "PERCENT_MEM" "TIME" "COMM" ];
+    fields =
+      if config.system.os != "Nix-on-Droid"
+        then [ "USER" "PERCENT_CPU" ]
+        else [ "PID" "STATE" ]  # at least it's something
+      ++ [ "PERCENT_MEM" "TIME" "COMM" ];
     headerMargin = false;
     hideThreads = true;
     hideUserlandThreads = true;
     highlightBaseName = true;
-    meters.left = [ "AllCPUs2" "Memory" "Swap" ];
-    meters.right = [ { kind = "Battery"; mode = 1; } "Uptime" "Tasks" "LoadAverage" ];
+    meters.left =
+      if config.system.os != "Nix-on-Droid"
+        then [ "AllCPUs2" ]
+        else []
+      ++ [ "Memory" "Swap" ];
+    meters.right =
+      if config.system.os != "Nix-on-Droid"
+        then [ { kind = "Battery"; mode = 1; } "Uptime" "Tasks" "LoadAverage"]
+        else [ "Tasks" ];
     shadowOtherUsers = true;
     showCpuFrequency = true;
     showProgramPath = false;
