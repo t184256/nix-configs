acefad86a0828313456ab50654f228a0bb87d4a7
Alexander Sosedkin <monk@unboiled.info>
2021-02-14 13:52:55 +0100

    user/rsync: include -t in rs

--- user/rsync.nix
+++ user/rsync.nix
@@ -2,5 +2,5 @@
 
 {
   home.packages = [ pkgs.rsync ];
-  home.wraplings.rs = "${pkgs.rsync}/bin/rsync -r --no-i-r --info=progress2";
+  home.wraplings.rs = "${pkgs.rsync}/bin/rsync -rt --no-i-r --info=progress2";
 }
