d546edc46eaff8e50895dccd7a34ba585903f95e
Alexander Sosedkin <monk@unboiled.info>
2021-06-02 03:50:24 +0200

    user/htop: update for 21.05

--- user/htop.nix
+++ user/htop.nix
@@ -2,28 +2,40 @@
 
 {
   imports = [ ./config/os.nix ];
-  programs.htop = {
-    enable = true;
-    colorScheme = 1;
-    fields =
+  programs.htop.enable = true;
+  programs.htop.settings = {
+    color_scheme = 1;
+    fields = with config.lib.htop.fields;
       if config.system.os != "Nix-on-Droid"
-        then [ "USER" "PERCENT_CPU" "PERCENT_MEM" "TIME" "COMM" ]
-        else [ "PID" "STATE" "PERCENT_MEM" "TIME" "COMM" ];
-    headerMargin = false;
-    hideThreads = true;
-    hideUserlandThreads = true;
-    highlightBaseName = true;
-    meters.left =
-      if config.system.os != "Nix-on-Droid"
-        then [ "AllCPUs2" "Memory" "Swap" ]
-        else [ "Memory" "Swap" ];
-    meters.right =
-      if config.system.os != "Nix-on-Droid"
-        then [ { kind = "Battery"; mode = 1; } "Uptime" "Tasks" "LoadAverage"]
-        else [ "Tasks" ];
-    shadowOtherUsers = true;
-    showCpuFrequency = true;
-    showProgramPath = false;
-    sortKey = "PERCENT_MEM";
-  };
+        then [ USER PERCENT_CPU PERCENT_MEM TIME COMM ]
+        else [ PID STATE PERCENT_MEM TIME COMM ] ;
+    header_margin = false;
+    hide_threads = true;
+    hide_userland_threads = true;
+    highlight_base_name = true;
+    shadow_other_users = true;
+    show_cpu_frequency = true;
+    show_program_path = false;
+    sort_key = "PERCENT_MEM";
+  } // (with config.lib.htop; leftMeters (
+    if config.system.os != "Nix-on-Droid"
+    then {
+      AllCPUs2 = modes.Bar;
+      Memory = modes.Bar;
+      Swap = modes.Bar;
+    } else {
+      Memory = modes.Bar;
+      Swap = modes.Bar;
+    }
+  )) // (with config.lib.htop; rightMeters (
+    if config.system.os != "Nix-on-Droid"
+    then {
+      Battery = modes.Bar;
+      Uptime = modes.Text;
+      Tasks = modes.Text;
+      LoadAverage = modes.Text;
+    } else {
+      Tasks = modes.Text;
+    }
+  ));
 }
