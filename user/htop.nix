4fea085b677f216031b0a66837cf82cbd8801ccb
Alexander Sosedkin <monk@unboiled.info>
2021-06-06 19:22:37 +0200

    user/htop: various fixes

--- user/htop.nix
+++ user/htop.nix
@@ -11,12 +11,14 @@
         else [ PID STATE PERCENT_MEM TIME COMM ] ;
     header_margin = false;
     hide_threads = true;
+    hide_kernel_threads = true;
     hide_userland_threads = true;
     highlight_base_name = true;
+    highlight_megabytes = true;
     shadow_other_users = true;
     show_cpu_frequency = true;
     show_program_path = false;
-    sort_key = "PERCENT_MEM";
+    sort_key = config.lib.htop.fields.PERCENT_MEM;
   } // (with config.lib.htop; leftMeters (
     if config.system.os != "Nix-on-Droid"
     then {
