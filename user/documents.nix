831c1f73d5e787c50083621215de8a3db7554eef
Alexander Sosedkin <monk@unboiled.info>
2021-06-10 18:50:37 +0200

    user/documents: init

---
+++ user/documents.nix
@@ -0,0 +1,10 @@
+{ pkgs, config, lib, ... }:
+
+{
+  imports = [ ./config/no-graphics.nix ];
+
+  home.packages = lib.mkIf (! config.system.noGraphics) [
+    pkgs.libreoffice
+    pkgs.xournalpp
+  ];
+}
