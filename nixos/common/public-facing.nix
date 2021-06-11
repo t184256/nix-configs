698bab2359616c26851ace96aaddb95443290798
Alexander Sosedkin <monk@unboiled.info>
2021-06-11 23:49:09 +0200

    nixos/common/public-facing: open syncthing ports

--- nixos/common/public-facing.nix
+++ nixos/common/public-facing.nix
@@ -1,5 +1,10 @@
 {
+  services.sshguard.enable = true;
+
   # mosh
   networking.firewall.allowedUDPPortRanges = [ { from = 60000; to = 61000; } ];
-  services.sshguard.enable = true;
+
+  # syncthing
+  networking.firewall.allowedTCPPorts = [ 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }
