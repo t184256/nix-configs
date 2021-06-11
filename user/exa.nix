678bd96078a5c173daa0109f2b2f29788641000e
Alexander Sosedkin <monk@unboiled.info>
2021-06-12 01:52:38 +0200

    user/exa: update

--- user/exa.nix
+++ user/exa.nix
@@ -4,7 +4,7 @@
   imports = [ ./wraplings.nix ./config/os.nix ];
 
   # calling exa directly
-  home.packages = [ pkgs.exa ];
+  programs.exa.enable = true;
 
   # calling exa via wraplings
   home.wraplings = rec {
@@ -14,5 +14,6 @@
     );
     la = l + " -a";
     lt = l + " -T";
+    lla = l + " -la";
   };
 }
