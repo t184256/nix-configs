bf06a713be9b2b4491632946d1589849f5b0c325
Alexander Sosedkin <monk@unboiled.info>
2021-06-19 01:50:19 +0200

    user/documents: add inkscape

--- user/documents.nix
+++ user/documents.nix
@@ -3,8 +3,9 @@
 {
   imports = [ ./config/no-graphics.nix ];
 
-  home.packages = lib.mkIf (! config.system.noGraphics) [
-    pkgs.libreoffice
-    pkgs.xournalpp
-  ];
+  home.packages = lib.mkIf (! config.system.noGraphics) (with pkgs; [
+    libreoffice
+    inkscape
+    xournalpp
+  ]);
 }
