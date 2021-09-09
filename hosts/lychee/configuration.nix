1d7d082ce3c2430b9b71accb504b989dfc2f00e6
Alexander Sosedkin <monk@unboiled.info>
2021-09-09 08:53:18 +0200

    hosts/lychee: update kernel to 5.14

--- hosts/lychee/configuration.nix
+++ hosts/lychee/configuration.nix
@@ -8,7 +8,7 @@
   ];
 
 
-  boot.kernelPackages = pkgs.linuxPackages_5_13;
+  boot.kernelPackages = pkgs.linuxPackages_5_14;
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 2;  # small /boot
   boot.loader.efi.canTouchEfiVariables = true;
