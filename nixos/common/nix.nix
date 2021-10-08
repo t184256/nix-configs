5417885256bc1cb793ecf98306938865784dd6a4
Alexander Sosedkin <monk@unboiled.info>
2021-10-08 23:37:32 +0200

    nixos: add nix-cache.unboiled.info as a substituter

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -3,6 +3,8 @@
 {
   nix.package = pkgs.nixFlakes;
   nix.extraOptions = ''
+    extra-substituters = https://nix-cache.unboiled.info
+    trusted-public-keys = nix-cache.unboiled.info-1:P/F71h2Fc7jfhxsoefISVYBfq0vALOMCIxEEmvtmpMg= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
     experimental-features = nix-command flakes
     builders-use-substitutes = true
     log-lines = 20
