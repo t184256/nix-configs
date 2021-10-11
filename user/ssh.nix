ae432925190b53ef16972962da29cad60ade58f2
Alexander Sosedkin <monk@unboiled.info>
2021-10-11 14:40:55 +0200

    user.ssh: add flaky

--- user/ssh.nix
+++ user/ssh.nix
@@ -5,6 +5,8 @@
     enable = true;
     matchBlocks = {
       fig = { user = "monk"; hostname = "fig.unboiled.info"; };
+      flaky = { user = "monk"; hostname = "192.168.100.220";
+                extraOptions = {ProxyJump = "fig";}; };
       mango = { user = "monk"; hostname = "mango.unboiled.info"; };
       duckweed = { user = "monk"; hostname = "duckweed.unboiled.info"; };
       jungle = { user = "root"; hostname = "jungle.lan"; };
