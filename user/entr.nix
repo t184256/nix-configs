98784b3a823509e7942748c319643e01cab94afe
Alexander Sosedkin <monk@unboiled.info>
2021-11-04 19:21:39 +0100

    user/entr: add 'en' and 'wat' wraplings

--- user/entr.nix
+++ user/entr.nix
@@ -1,5 +1,16 @@
 { pkgs, config, ... }:
 
 {
-  home.packages = [ pkgs.entr ];
+  home.packages = [
+    pkgs.entr
+
+    (pkgs.writeShellScriptBin "en" ''
+      exec find "''${@:2}" | ${pkgs.entr}/bin/entr -rcs "$1"
+    '')
+
+    (pkgs.writeShellScriptBin "wat" ''
+      exec ${pkgs.findutils}/bin/find ''${*%''${!#}} | \
+        ${pkgs.entr}/bin/entr -rcs "''${@:$#}"
+    '')
+  ];
 }
