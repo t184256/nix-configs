367140d2b6c49edc402d89ef204852b75e083cff
Alexander Sosedkin <monk@unboiled.info>
2021-12-18 14:05:22 +0100

    nixos/common/nix: tune my cache's priority

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -3,7 +3,7 @@
 {
   nix.package = pkgs.nixFlakes;
   nix.extraOptions = ''
-    extra-substituters = https://nix-cache.unboiled.info
+    extra-substituters = https://nix-cache.unboiled.info?priority=10
     trusted-public-keys = nix-cache.unboiled.info-1:P/F71h2Fc7jfhxsoefISVYBfq0vALOMCIxEEmvtmpMg= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
     experimental-features = nix-command flakes
     builders-use-substitutes = true
