f49c428ead8f565eff96be89f286eced8c495cdd
Alexander Sosedkin <monk@unboiled.info>
2022-04-10 03:16:35 +0200

    hosts/duckweed: impermanence + tmpfs root

--- hosts/duckweed/configuration.nix
+++ hosts/duckweed/configuration.nix
@@ -19,14 +19,45 @@
 
   nix.autoOptimiseStore = true;  # it's tight on disk space
 
+  users.mutableUsers = false;
   users.users.monk = {
     isNormalUser = true;
     extraGroups = [ "wheel" ];
+    passwordFile = "/mnt/persist/passwords/monk";
   };
+  users.users.root.passwordFile = "/mnt/persist/passwords/root";
 
   system.noGraphics = true;
   home-manager.users.monk.system.noGraphics = true;
 
   system.stateVersion = "21.05";
   home-manager.users.monk.home.stateVersion = "21.05";
+
+  environment.persistence."/mnt/persist" = {
+    hideMounts = true;
+    directories = [
+      "/var/lib/private/syncthing-relay"
+      "/var/log"
+    ];
+    files =
+      let
+        mode = { mode = "0700"; };
+      in
+      [
+        "/etc/machine-id"
+        { file = "/etc/ssh/ssh_host_rsa_key"; parentDirectory = mode; }
+        { file = "/etc/ssh/ssh_host_rsa_key.pub"; parentDirectory = mode; }
+        { file = "/etc/ssh/ssh_host_ed25519_key"; parentDirectory = mode; }
+        { file = "/etc/ssh/ssh_host_ed25519_key.pub"; parentDirectory = mode; }
+      ];
+    users.monk = {
+      directories = [
+        ".local/share/pygments-cache"
+        ".local/share/xonsh"
+      ];
+      files = [
+        ".bash_history"
+      ];
+    };
+  };
 }
--- hosts/duckweed/hardware-configuration.nix
+++ hosts/duckweed/hardware-configuration.nix
@@ -2,5 +2,17 @@
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   boot.initrd.kernelModules = [ "nvme" ];
-  fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
+
+  fileSystems."/" = {
+    device = "none";
+    fsType = "tmpfs";
+    options = [ "defaults" "size=256M" "mode=755" ];
+    neededForBoot = true;
+  };
+  fileSystems."/mnt/persist" = {
+    device = "/dev/vda1";
+    fsType = "ext4";
+    neededForBoot = true;
+  };
+  fileSystems."/nix" = { device = "/mnt/persist/nix"; options = [ "bind" ]; };
 }
