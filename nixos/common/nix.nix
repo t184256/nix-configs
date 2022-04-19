0511e427620333242faddefd7e255696fe7f28e6
Alexander Sosedkin <monk@unboiled.info>
2022-04-19 14:44:54 +0200

    nixos/common/nix: lower my cache's priority

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -4,7 +4,7 @@
   nix.package = pkgs.nixFlakes;
   nix.extraOptions = ''
     trusted-users = monk
-    extra-substituters = https://nix-cache.unboiled.info?priority=10
+    extra-substituters = https://nix-cache.unboiled.info?priority=100
     trusted-public-keys = nix-cache.unboiled.info-1:P/F71h2Fc7jfhxsoefISVYBfq0vALOMCIxEEmvtmpMg= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
     experimental-features = nix-command flakes ca-derivations
     builders-use-substitutes = true
