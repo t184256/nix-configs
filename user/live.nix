08159681ea79ce8f2c6ee3a2dda1b117d7d89f4c
Alexander Sosedkin <monk@unboiled.info>
2022-07-08 15:47:14 +0200

    hosts/cookie: fix live-network

--- user/live.nix
+++ user/live.nix
@@ -15,8 +15,8 @@ let
     fi
 
     ${pkgs.gnupg}/bin/gpg -d /tmp/secrets.7z.gpg > /tmp/secrets.7z
-    mkdir /tmp/secrets
-    pushd /tmp/secrets
+    mkdir /tmp
+    pushd /tmp
       ${pkgs.p7zip}/bin/7z x /tmp/secrets.7z
     popd
     sudo cp -r /tmp/secrets/*.nmconnection \
@@ -25,6 +25,14 @@ let
     sudo chmod -R 600 /etc/NetworkManager/system-connections
     sudo chmod 700 /etc/NetworkManager/system-connections
     sudo systemctl restart NetworkManager
+    if [[ -e /tmp/secrets/rhca.pem ]] && [[ -e /tmp/secrets/rhvpn.ovpn ]]; then
+      sudo cp /tmp/secrets/rhca.pem /etc/pki/tls/certs/2015-RH-IT-Root-CA.pem
+      sudo chown root:root /etc/pki/tls/certs/2015-RH-IT-Root-CA.pem
+      sudo chmod 640 /etc/pki/tls/certs/2015-RH-IT-Root-CA.pem
+      sudo cp rhvpn.ovpn /etc/NetworkManager/system-connections/rhvpn.ovpn
+      sudo chown root:root /etc/NetworkManager/system-connections/rhvpn.ovpn
+      sudo chmod 600 /etc/NetworkManager/system-connections/rhvpn.ovpn
+    fi
     rm -r /tmp/secrets.7z
     rm -r /tmp/secrets
     touch /tmp/.network-configured
