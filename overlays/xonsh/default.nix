c9eea5bee82f515f6fdd043d7d9f00f4a22ca8e7
Alexander Sosedkin <monk@unboiled.info>
2022-07-29 22:49:41 +0200

    overlays/xonsh: update xonsh

--- overlays/xonsh/default.nix
+++ overlays/xonsh/default.nix
@@ -8,23 +8,24 @@ let
   ptk = super.python3Packages.prompt_toolkit.overridePythonAttrs ({
     patches = [
       (super.fetchpatch {
-        url = "https://github.com/prompt-toolkit/python-prompt-toolkit/pull/1591.patch";
+        url = "https://github.com/prompt-toolkit/python-prompt-toolkit/commit/e5a86e270e7ee698c849d39196d5a6a0d6e5a331.patch";
         sha256 = "sha256-KbM1FECVs+6MKyvEOTzyfR5s825ejunr4q2blJXOmJ0=";
       })
     ];
-    patchFlags = [ "-p2" ];
+    patchFlags = [ "-p1" ];
+    doCheck = false;
   });
 
   xonshLib = super.python3Packages.buildPythonPackage rec {
     inherit (super.xonsh) postPatch
                           meta shellPath;
     pname = "xonsh";
-    version = "0.12.1";
+    version = "0.13.0";
     src = super.fetchFromGitHub {
       owner = "xonsh";
       repo = "xonsh";
-      rev = "337cf25a40e18e95050e812a1d1b7ac8bf6f3335";
-      sha256 = "sha256-jfxQMEVABTOhx679V0iGVX9RisuY42lSdztYXMLwdcw=";
+      rev = version;
+      sha256 = "sha256-8X/+mQrwJ0yaUHRKdoY3G0P8kq22hYfRK+7WRl/zamc=";
     };
     propagatedBuildInputs = with super.python3Packages; [
       ply
@@ -48,6 +49,8 @@ let
       "test_colorize_file"
       "test_loading_correctly"
       "test_no_command_path_completion"
+      "test_bsd_man_page_completions"
+      "test_xonsh_activator"
       # fails on non-interactive shells
       "test_capture_always"
       "test_casting"
@@ -55,8 +58,13 @@ let
       "test_dirty_working_directory"
       "test_man_completion"
       "test_vc_get_branch"
+      "test_bash_and_is_alias_is_only_functional_alias"
     ];
     disabledTestPaths = [
+      # fails on sandbox
+      "tests/completers/test_command_completers.py"
+      "tests/test_ptk_highlight.py"
+      "tests/test_ptk_shell.py"
       # fails on non-interactive shells
       "tests/prompt/test_gitstatus.py"
       "tests/completers/test_bash_completer.py"
@@ -64,10 +72,6 @@ let
     postInstall = ''
       site_packages=$(python -c "import site; print(site.__file__.rsplit('/', 2)[-2])")
       xonsh=$out/lib/$site_packages/site-packages/xonsh/
-      install -D -m644 xonsh/parser_table.py xonsh/__amalgam__.py $xonsh
-      install -D -m644 xonsh/completers/__amalgam__.py $xonsh/completers/
-      install -D -m644 xonsh/history/__amalgam__.py $xonsh/history/
-      install -D -m644 xonsh/prompt/__amalgam__.py $xonsh/prompt/
       python -m compileall --invalidation-mode unchecked-hash $xonsh
     '';
   };
