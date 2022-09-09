1bf5ddc7269aaaa9b15201abfdfe0e763b76ac3d
Alexander Sosedkin <monk@unboiled.info>
2022-09-09 21:46:51 +0200

    nixos/services/podcastify: keep tweaking

--- nixos/services/podcastify.nix
+++ nixos/services/podcastify.nix
@@ -14,8 +14,13 @@
       forceSSL = true;
       locations."/".proxyPass = "http://127.0.0.1:9696";
       extraConfig = ''
+        gzip off;
+        gzip_proxied off;
+        proxy_cache off;
+        proxy_buffering off;
         proxy_connect_timeout 120;
         proxy_read_timeout 180;
+        proxy_send_timeout 180;
       '';
     };
   };
