399f6ba7657cc86d09f28fda3163dccc7244dd97
Alexander Sosedkin <monk@unboiled.info>
2022-12-23 11:09:53 +0100

    nixos/common/tweaks: DefaultTimeoutStopSec=30s

--- nixos/common/tweaks.nix
+++ nixos/common/tweaks.nix
@@ -3,5 +3,8 @@
 {
   boot.tmpOnTmpfs = lib.mkDefault true;
   systemd.enableEmergencyMode = false;
+  systemd.extraConfig = ''
+    DefaultTimeoutStopSec=30s
+  '';
   services.journald.extraConfig = "SystemMaxUse=5%";
 }
