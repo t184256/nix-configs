5f2e4738030972b0fe40b9934a49f79275a98c60
Alexander Sosedkin <monk@unboiled.info>
2023-07-15 05:37:43 +0200

    hosts/loquat: more service-to-core pinning

--- hosts/loquat/configuration.nix
+++ hosts/loquat/configuration.nix
@@ -61,7 +61,10 @@
     MemoryHigh = "14G"; MemoryMax = "15G";
   };
   systemd.services.yousable-back.serviceConfig.CPUAffinity = "0-3";
+  systemd.services.syncthing.serviceConfig.CPUAffinity = "0-3";
+  systemd.services.gitea.serviceConfig.CPUAffinity = "0-3";
   systemd.services.akkoma.serviceConfig.CPUAffinity = "0-2";
+  systemd.services.lemmy.serviceConfig.CPUAffinity = "0-2";
 
   home-manager.users.monk.language-support = [ "nix" "bash" ];
 
