c465e232874dd9e7df63b3f5ebdf5b35161a86bd
Alexander Sosedkin <monk@unboiled.info>
2023-08-12 05:09:17 +0200

    nixos/services/postgresql: enable local backups

--- nixos/services/postgresql.nix
+++ nixos/services/postgresql.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   environment.persistence."/mnt/persist".directories = [
@@ -6,5 +6,15 @@
       directory = "/var/lib/postgresql";
       user = "postgres"; group = "postgres";
     }
+    {
+      directory = "/var/backup/postgresql";
+      user = "postgres"; group = "postgres";
+    }
   ];
+  services.postgresqlBackup = {
+    enable = config.services.postgresql.enable;
+    compression = "zstd";
+    compressionLevel = 12;
+    startAt = "*-*-* 04:52:00";
+  };
 }
