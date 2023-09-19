657b559adbceb211b9f2f9fe4247622443d71b76
Alexander Sosedkin <monk@unboiled.info>
2023-09-19 22:20:33 +0200

    nixos/common/disable-cruft: disable most networkmanager plugins

--- nixos/common/disable-cruft.nix
+++ nixos/common/disable-cruft.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ pkgs, lib, ... }:
 
 {
   programs.command-not-found.enable = false;
   documentation.info.enable = false;
   documentation.nixos.enable = false;
   environment.defaultPackages = [];
+  networking.networkmanager.plugins = lib.mkForce (with pkgs; [
+    # openconnect specifically pulls in graphics and sound. ugh.
+    networkmanager-iodine
+    networkmanager-openvpn
+  ]);
 }
