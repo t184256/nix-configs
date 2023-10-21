561dd12e35162b8373089c80da163e545339020f
Alexander Sosedkin <monk@unboiled.info>
2023-10-21 04:30:33 +0200

    user/neovim/treesitter: avoid installing (gcc, nodejs)

--- user/neovim/treesitter.nix
+++ user/neovim/treesitter.nix
@@ -1,13 +1,14 @@
-{ pkgs, ... }:
+{ config, ... }:
 
 {
   programs.nixvim.plugins.treesitter = {
     # fancy plugins: treesitter
     # playground  # :TSHighlightCapturesUnderCursor,
                   # :help treesitter-highlight-groups
-    enable = true;
+    enable = config.language-support != [];
     moduleConfig.highlight = {
       enable = true;
+      indent = true;
       #additional_vim_regex_highlighting = false;
     };
     #vim.api.nvim_set_hl(0, "@none", { link = "Normal" })
