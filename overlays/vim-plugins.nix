59c72d07e536ca72828f7a8030e069cedf4ac758
Alexander Sosedkin <monk@unboiled.info>
2024-02-29 09:25:32 +0100

    overlays/vim-plugins: update code-actions

--- overlays/vim-plugins.nix
+++ overlays/vim-plugins.nix
@@ -26,12 +26,12 @@ self: super:
 
     actions-preview = super.pkgs.vimUtils.buildVimPlugin {
       pname = "actions-preview";
-      version = "2023-05-12";
+      version = "2024-02-25";
       src = super.fetchFromGitHub {
         owner = "aznhe21";
         repo = "actions-preview.nvim";
-        rev = "3028c9a35853bb5fb77670fb58537ce28085329c";
-        sha256 = "sha256-mkLn2/klAdirbqxJ3xLz2vyjEx4Sb0NLEK/LS2w8rag=";
+        rev = "5c240e6356156b72cfbf7c05feefadae61d7a06f";
+        sha256 = "sha256-XJxwKusvtKvLdtvOjLwmwCP09djhjm9DqL4C1Eca4oY=";
       };
     };
   };
