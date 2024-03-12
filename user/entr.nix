6b2c5e9581600e688a7a213dc47a38c6ce956fc7
Alexander Sosedkin <monk@unboiled.info>
2024-03-12 16:06:06 +0100

    user/entr: tweak watpy

--- user/entr.nix
+++ user/entr.nix
@@ -1,28 +1,45 @@
-{ pkgs, config, ... }:
+{ pkgs, ... }:
 
+let
+  testpy = pkgs.writeShellScriptBin "testpy" ''
+    set -ue
+    ruff check . || true
+    COV_OPTS=""
+    if grep -q pytest-cov flake.nix 2>/dev/null || \
+        grep -q pytest-cov pyproject.toml 2>/dev/null; then
+      COV_OPTS="--no-cov-on-fail --cov-report term-missing:skip-covered"
+    fi
+
+    python -m pytest $COV_OPTS \
+                     --ff \
+                     --durations=3 --durations-min=.05 \
+                     --tb=short \
+                     --no-header \
+                     "$@"
+    if [ -e .pre-commit-config.yaml ]; then
+      pre-commit run -a
+    else
+      ruff check . && ruff format --check .
+    fi
+    echo ok
+  '';
+in
 {
   home.packages = [
     pkgs.entr
 
     (pkgs.writeShellScriptBin "wat" ''
       exec ${pkgs.findutils}/bin/find ''${*%''${!#}} \
-      | grep -vF '/cassettes/' \
-      | grep -vE '(__pycache__/|\.pyc)$' \
+      | grep -vF './.git' \
+      | grep -vE '(__pycache__|\.pyc)$' \
+      | grep -vE '^(./|)\.(mypy|ruff|pytest)_cache/' \
       | grep -vE '^(./|)htmlcov/' \
+      | grep -vF '/cassettes/' \
       | ${pkgs.entr}/bin/entr -rcs "''${@:$#}"
     '')
 
     (pkgs.writeShellScriptBin "watpy" ''
-      exec wat * "ruff check .; \
-                  python -m pytest --ff --no-cov-on-fail \
-                                   --cov-report term-missing:skip-covered \
-                                   --durations=3 --durations-min=.05 \
-                                   --tb=short \
-                                   --no-header \
-                                   $* && \
-                  ruff check . && \
-                  ruff format --check . && \
-                  echo ok"
+      exec wat . ${testpy}/bin/testpy
     '')
   ];
 }
