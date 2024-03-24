eda48a7150354ae80826c17ea05e0a93e0bf80b2
Alexander Sosedkin <monk@unboiled.info>
2024-03-24 03:58:51 +0100

    user/neovim/tabby: `ai` autocompletes, `vi` has it manual

--- user/neovim/tabby.nix
+++ user/neovim/tabby.nix
@@ -6,8 +6,10 @@
       vim-tabby
     ];
     extraConfigLua = ''
+      vim.g.tabby_trigger_mode = 'manual'
       vim.g.tabby_keybinding_accept = '<C-Space>'
       vim.g.tabby_keybinding_trigger_or_dismiss = '<C-_>'
     '';
   };
+  home.wraplings.ai = "nvim '+lua vim.g.tabby_trigger_mode = \"auto\"'";
 }
