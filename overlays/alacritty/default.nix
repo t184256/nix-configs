274b680ddeddca82b85f62fcf7561a9aef2be33c
Alexander Sosedkin <monk@unboiled.info>
2024-05-07 18:55:42 +0200

    overlays/alacritty: update

--- overlays/alacritty/default.nix
+++ overlays/alacritty/default.nix
@@ -2,12 +2,12 @@ _: super:
 
 {
   alacritty = super.alacritty.overrideAttrs (_: {
-    version = "0.13.1-master-2024-03-24-t184256-altfont";
+    version = "0.13.2-master-2024-05-03-t184256-altfont";
     src = super.fetchFromGitHub {
       owner = "t184256";
       repo = "alacritty";
-      rev = "5744f32e7d845ca1aeb18daa34d867efc2fc3f6b";
-      sha256 = "0pygrzpknvsxdxjfiq4krv58x9y72zfwr65d81b1x2vqnszgi8hf";
+      rev = "4c1504423d9ef48d5ae31ba683a536e91495d24a";
+      sha256 = "0rjxhq3w99wn89rwh133f0s2h5fbmxd6m04205j5w2v83fikgs0r";
     };
     cargoDeps = super.rustPlatform.importCargoLock {
       lockFile = ./Cargo.lock;
