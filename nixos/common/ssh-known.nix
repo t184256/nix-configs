b247d6d724d8abd7fd120a0069f2ff3732f1cb3b
Alexander Sosedkin <monk@unboiled.info>
2024-06-23 01:03:51 +0200

    nixos/common/ssh-known: add

---
+++ nixos/common/ssh-known.nix
@@ -0,0 +1,6 @@
+{
+  programs.ssh.knownHosts = {
+    "loquat.unboiled.info".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKBra/oIenaBvFOtNsF97uUrWRakng1R1zcJyy0/MoR8";
+    "duckweed.unboiled.info".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIASTR5SavefPqRkeTrI6bbMNp6AFnf4wHA5Lxo72H/wJ";
+  };
+}
