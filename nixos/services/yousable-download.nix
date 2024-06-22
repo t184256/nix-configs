c2b51040e6dc947a7c43dd44d71eb8b292bd81b7
Alexander Sosedkin <monk@unboiled.info>
2024-06-23 01:04:06 +0200

    nixos/services/yousable-download: add sshfs mount

--- nixos/services/yousable-download.nix
+++ nixos/services/yousable-download.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.yousable = {
@@ -8,6 +8,24 @@
     server.enable = false;
     configFile = "/etc/yousable/config.yaml";
   };
+  systemd = {
+    services.yousable-crawler = {
+      requires = [ "remote-yousable.mount" ];
+      after = [ "remote-yousable.mount" ];
+    };
+    services.yousable-downloader = {
+      requires = [ "remote-yousable.mount" ];
+      after = [ "remote-yousable.mount" ];
+    };
+    mounts = [ {
+      before = [ "remote-fs.target" ];
+      what = "yousable@loquat.unboiled.info:/mnt/persist/cache/yousable";
+      where = "/remote/yousable";
+      type = "fuse.sshfs";
+      options = "identityfile=/mnt/persist/secrets/yousable-ssh/id_ed25519,uid=yousable,gid=yousable,allow_other";
+    } ];
+  };
+  environment.systemPackages = with pkgs; [ sshfs ];
   environment.persistence."/mnt/persist".directories = [
     {
       directory = "/etc/yousable";
