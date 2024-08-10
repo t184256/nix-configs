321530bc305b10f53511f45e5f6b3e3f7a7b1d7f
Alexander Sosedkin <monk@unboiled.info>
2024-08-10 04:08:55 +0200

    hosts/loquat: disable fedifetcher

--- hosts/loquat/configuration.nix
+++ hosts/loquat/configuration.nix
@@ -7,7 +7,7 @@
     ./hardware-configuration.nix
     ../../nixos/services/akkoma.nix
     ../../nixos/services/dns.nix
-    ../../nixos/services/fedifetcher.nix
+    #../../nixos/services/fedifetcher.nix
     ../../nixos/services/ipfs/cluster-leader.nix
     ../../nixos/services/ipfs/node.nix
     ../../nixos/services/git.nix
