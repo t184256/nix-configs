f87ac0ec421a849668940f571b4271be198a3777
Alexander Sosedkin <monk@unboiled.info>
2024-08-18 01:32:22 +0200

    hosts/watermelon/network: guess IPv6

--- hosts/watermelon/network.nix
+++ hosts/watermelon/network.nix
@@ -3,10 +3,14 @@ _:
 {
   networking = {
     networkmanager.enable = false;
-    interfaces.enX0.ipv4.addresses = [
-      { address = "104.152.210.200"; prefixLength = 24; }
-    ];
+    interfaces.enX0 = {
+      ipv4.addresses =
+        [ { address = "104.152.210.200"; prefixLength = 24; } ];
+      ipv6.addresses =
+        [ { address = "2a02:c206:6898:d2c8::1"; prefixLength = 64; } ];
+    };
     defaultGateway = "104.152.210.1";
-    nameservers = [ "1.1.1.1" ];
+    defaultGateway6 = { address = "fe80::1"; interface = "enX0"; };
+    nameservers = [ "1.1.1.1" "2606:4700:4700::1111" ];
   };
 }
