4514db1607d7f9372ebce5f1727880da8a6c5aea
Alexander Sosedkin <monk@unboiled.info>
2024-08-27 23:02:04 +0200

    hosts/cocoa: add custom EDIDs wired nowhere

--- hosts/cocoa/configuration.nix
+++ hosts/cocoa/configuration.nix
@@ -62,4 +62,21 @@
   services.displayManager.autoLogin = { enable = true; user = "monk"; };
 
   boot.initrd.systemd.enable = true;
+
+  hardware.display.edid = {
+    enable = true;
+    modelines = {
+       "800x600" = "40 800 840 968 1056 600 601 605 628 -hsync +vsync";
+       "1920x1080" = "148.5 1920 2008 2052 2200 1080 1084 1089 1125 +hsync +vsync";
+       "2208x1786" = "246.006 2208 2216 2248 2288 1786 1823 1831 1837 +hsync -vsync ratio=16:9";
+       "2960x1848" = "468.06 2960 3192 3520 4080 1848 1849 1852 1912 -hsync +vsync ratio=16:9";
+       "2960x1848-12" = "988.42 2960 3224 3560 4160 1848 1849 1852 1980 -hsync +vsync ratio=16:9";
+       "3840x2160" = "509.443 3840 3848 3880 3920 2160 2208 2216 2222 +hsync -vsync ratio=16:9";
+       "3840x2160-3" = "339.57 3840 4080 4496 5152 2160 2161 2164 2197 -hsync +vsync ratio=16:9";
+    };
+    # If DP-1 has the plug,
+    # sudo tee >/dev/null /sys/kernel/debug/dri/1/DP-1/edid_override < /run/current-system/firmware/edid/1920x1080.bin
+    # echo off | sudo tee >/dev/null /sys/class/drm/card1-DP-1/status
+    # echo detect | sudo tee >/dev/null /sys/class/drm/card1-DP-1/status
+  };
 }
