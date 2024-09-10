8d779c2fe3cfae0d7dd2dcbac965a03e176e88ec
Alexander Sosedkin <monk@unboiled.info>
2024-09-10 12:38:37 +0200

    user/neovim/tabby: load less of tabby

--- user/neovim/tabby.nix
+++ user/neovim/tabby.nix
@@ -10,12 +10,16 @@
       vim-tabby
     ];
     extraConfigLua = ''
-      vim.g.tabby_trigger_mode = 'manual'
-      vim.g.tabby_keybinding_accept = '<C-Space>'
-      vim.g.tabby_keybinding_trigger_or_dismiss = '<C-_>'
+      if (vim.g.with_tabby ~= 1) then
+        vim.g.tabby_trigger_mode = 'auto'
+        vim.g.tabby_keybinding_accept = '<C-Space>'
+        vim.g.tabby_keybinding_trigger_or_dismiss = '<C-_>'
+      else
+        vim.g.loaded_tabby = 0  -- any value prevents loading
+      end
     '';
   };
   home.wraplings = if (! config.neovim.fat) then {} else {
-    ai = "nvim '+lua vim.g.tabby_trigger_mode = \"auto\"'";
+    ai = "nvim '+lua vim.g.want_tabby = 1'";
   };
 }
