e17e85bead071e4978ea567555942d92da718117
Alexander Sosedkin <monk@unboiled.info>
2024-09-19 07:56:11 +0200

    neovim/user/tabby: fix loading tabby

--- user/neovim/tabby.nix
+++ user/neovim/tabby.nix
@@ -7,19 +7,21 @@
   ];
   programs.nixvim = if (! config.neovim.fat) then {} else {
     extraPlugins = with pkgs.vimPlugins; [
-      vim-tabby
+      {
+        plugin = vim-tabby;
+        optional = true;
+      }
     ];
     extraConfigLua = ''
-      if (vim.g.with_tabby ~= 1) then
+      if (vim.g.with_tabby == 1) then
         vim.g.tabby_trigger_mode = 'auto'
         vim.g.tabby_keybinding_accept = '<C-Space>'
         vim.g.tabby_keybinding_trigger_or_dismiss = '<C-_>'
-      else
-        vim.g.loaded_tabby = 0  -- any value prevents loading
+        vim.api.nvim_command ":packadd vim-tabby"
       end
     '';
   };
   home.wraplings = if (! config.neovim.fat) then {} else {
-    ai = "nvim '+lua vim.g.want_tabby = 1'";
+    ai = "nvim --cmd 'lua vim.g.with_tabby = 1'";
   };
 }
