e4255f1bd2230a51e06872673ce79bdd13e91cf5
Alexander Sosedkin <monk@unboiled.info>
2024-09-30 07:59:43 +0200

    nixos/common/nix: remove my cache

--- nixos/common/nix.nix
+++ nixos/common/nix.nix
@@ -1,27 +1,20 @@
-{ pkgs, lib, config, ... }:
+_:
 
-let
-  notHydra = config.networking.hostName != "loquat";
-in
 {
-  nix.settings = {
-    substituters =
-      lib.mkIf notHydra [ "https://hydra.unboiled.info?priority=200" ];
-    trusted-public-keys = [
-      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
-    ] ++ (if notHydra then [
-      "hydra.unboiled.info-1:c7i8vKOB30a+DaJ2M04F0EM8CPRfU+WpbqWie4n221M="
-    ] else []);
+  nix = {
+    settings = {
+      substituters = [ "https://hydra.unboiled.info?priority=200" ];
+    };
+    settings.trusted-users = [ "monk" ];
+    extraOptions = ''
+      experimental-features = nix-command flakes cgroups ca-derivations
+      use-cgroups = true
+      builders-use-substitutes = true
+      narinfo-cache-negative-ttl = 300
+      narinfo-cache-positive-ttl = 7200
+      log-lines = 20
+    '';
+    daemonCPUSchedPolicy = "batch";
+    daemonIOSchedClass = "idle";
   };
-  nix.settings.trusted-users = [ "monk" ];
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes cgroups ca-derivations
-    use-cgroups = true
-    builders-use-substitutes = true
-    narinfo-cache-negative-ttl = 300
-    narinfo-cache-positive-ttl = 7200
-    log-lines = 20
-  '';
-  nix.daemonCPUSchedPolicy = "batch";
-  nix.daemonIOSchedClass = "idle";
 }
