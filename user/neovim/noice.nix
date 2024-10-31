6b01766e016a35616d2be95c9990104dfd896d34
Alexander Sosedkin <monk@unboiled.info>
2024-10-31 17:15:29 +0100

    flake.lock, user/neovim/noice: partial update

--- user/neovim/noice.nix
+++ user/neovim/noice.nix
@@ -7,28 +7,30 @@
 
     plugins.noice = {
       enable = true;
-      cmdline.view = "cmdline";
-      cmdline.format = {
-        cmdline = { icon = false; conceal = false; };
-        search_down = { icon = false; conceal = false; };
-        search_up = { icon = false; conceal = false; };
-        filter = { icon = false; conceal = false; };
-        lua = { icon = false; conceal = false; };
-        help = { icon = false; conceal = false; };
+      settings = {
+        cmdline.view = "cmdline";
+        cmdline.format = {
+          cmdline = { icon = false; conceal = false; };
+          search_down = { icon = false; conceal = false; };
+          search_up = { icon = false; conceal = false; };
+          filter = { icon = false; conceal = false; };
+          lua = { icon = false; conceal = false; };
+          help = { icon = false; conceal = false; };
+        };
+        lsp = {
+          signature.enabled = true;
+          progress.enabled = false;
+          signature.view = "virtualtext";
+          hover.view = "virtualtext";
+          documentation.view = "virtualtext";
+        };
+        views.mini = { position.row = "100%"; zindex = 50; };
+        # replace confirmation shouldn't obscure the text it's asking about
+        routes = [{
+          view = "cmdline";
+          filter.any = [ { event = "msg_show"; kind = "confirm_sub"; } ];
+        }];
       };
-      lsp = {
-        signature.enabled = true;
-        progress.enabled = false;
-        signature.view = "virtualtext";
-        hover.view = "virtualtext";
-        documentation.view = "virtualtext";
-      };
-      views.mini = { position.row = "100%"; zindex = 50; };
-      # replace confirmation shouldn't obscure the text it's asking about
-      routes = [{
-        view = "cmdline";
-        filter.any = [ { event = "msg_show"; kind = "confirm_sub"; } ];
-      }];
     };
 
     # z-index helps cmdline overlay the last line; hide the rest anyway
