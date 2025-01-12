ec717a404287fe90978fa243c29c367a85669a90
Alexander Sosedkin <monk@unboiled.info>
2025-01-12 22:39:43 +0100

    overlays/yt-dlp: update

--- overlays/yt-dlp.nix
+++ overlays/yt-dlp.nix
@@ -1,28 +1,72 @@
-_: prev:
+final: prev:
+# Doesn't work with non-default python version
 
 let
-  newerVer = "2024.12.23";
-  overrides = _: {
+  newerVer = "2024.12.25";
+  overrides-fresh = _: {
     name = "yt-dlp-${newerVer}";
     version = newerVer;
     src = prev.fetchFromGitHub {
       owner = "yt-dlp";
       repo = "yt-dlp";
-      rev = "2024.12.23";
-      hash = "sha256-6zv2NqbUxirMwa6OCzeKXuqLB4sFiYAcy3TYke2jhKc=";
+      #rev = "2024.12.23";
+      rev = "e2ef4fece6c9742d1733e3bae408c4787765f78c";
+      hash = "sha256-HfmdAI6uLfxDbbAhe8CROTGZ2IBa4i9xqSNWIFOwj04=";
     };
+  };
+  overrides-plugins = oa: {
+    propagatedBuildInputs = (oa.propagatedBuildInputs or []) ++ [
+      yt-dlp-get-pot
+      bgutil-ytdlp-pot-provider
+    ];
+  };
+  overrides-fresh-plugins = oa: (overrides-fresh oa) // (overrides-plugins oa);
+  yt-dlp = prev.yt-dlp.overridePythonAttrs (
+    if prev.lib.versionAtLeast prev.yt-dlp.version newerVer
+    then overrides-plugins
+    else overrides-fresh-plugins
+  );
+  yt-dlp-get-pot = prev.python3Packages.buildPythonPackage rec {
+    pname = "yt-dlp-get-pot";
+    version = "0.2.0";
+    pyproject = true;
+    src = prev.fetchFromGitHub {
+      owner = "coletdjnz";
+      repo = "yt-dlp-get-pot";
+      rev = "v${version}";
+      hash = "sha256-c5iKnZ7rYckbqvEI20nymOV6/QJAWyu/FX0QM6ps2D4=";
+    };
+    build-system = [ prev.python3Packages.hatchling ];
+    doCheck = false;
+    pythonImportsCheck = [ "yt_dlp_plugins" ];
+  };
+  bgutil-ytdlp-pot-provider = prev.python3Packages.buildPythonPackage rec {
+    pname = "bgutil-ytdlp-pot-provider";
+    version = "0.7.2";
+    pyproject = true;
+    src = prev.fetchFromGitHub {
+      owner = "Brainicism";
+      repo = "bgutil-ytdlp-pot-provider";
+      rev = version;
+      hash = "sha256-IiPle9hZEHFG6bjMbe+psVJH0iBZXOMg3pjgoERH3Eg=";
+    };
+    propagatedBuildInputs = [ yt-dlp-get-pot ];
+    postUnpack = "pwd; ls; cp source/README.md source/plugin/";
+    sourceRoot = "source/plugin";
+    build-system = [ prev.python3Packages.hatchling ];
+    doCheck = false;
+    pythonImportsCheck = [ "yt_dlp_plugins" ];
   };
 in
 {
-  pythonPackagesExtensions =
-    prev.pythonPackagesExtensions ++ [(_: pyPrev: { yt-dlp =
-      if prev.lib.versionAtLeast pyPrev.yt-dlp.version newerVer
-      then pyPrev.yt-dlp
-      else pyPrev.yt-dlp.overridePythonAttrs overrides;
-    })];
+  pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+    (_: pyPrev: {
+        yt-dlp = pyPrev.toPythonModule yt-dlp;
+        #yt-dlp = builtins.trace yt-dlp pyPrev.toPythonModule (yt-dlp.override {
+        #  python3Packages = pyFinal;
+        #});
+    })
+  ];
 
-  yt-dlp =
-    if prev.lib.versionAtLeast prev.yt-dlp.version newerVer
-    then prev.yt-dlp
-    else prev.yt-dlp.overrideAttrs overrides;  # not the best way, loses share/
+  inherit yt-dlp;
 }
