c73fd9bb5cafba6b7acecc8a891d74bd32e401a2
Alexander Sosedkin <monk@unboiled.info>
2025-01-26 07:34:46 +0100

    overlays/yt-dlp: update yt-dlp to 2025.01.26

--- overlays/yt-dlp.nix
+++ overlays/yt-dlp.nix
@@ -2,15 +2,15 @@ final: prev:
 # Doesn't work with non-default python version
 
 let
-  newerVer = "2025.01.15";
+  newerVer = "2025.01.26";
   overrides-fresh = _: {
     name = "yt-dlp-${newerVer}";
     version = newerVer;
     src = prev.fetchFromGitHub {
       owner = "yt-dlp";
       repo = "yt-dlp";
-      rev = "2025.01.15";
-      hash = "sha256-d9RGlANDkjRInIVADCrzx8+wn4wVc04J7Wn0c1q4N7k=";
+      rev = "2025.01.26";
+      hash = "sha256-bjvyyCvUpZNGxkFz2ce6pXDSKXJROKZphs9RV4CBs5M=";
     };
   };
   overrides-plugins = oa: {
