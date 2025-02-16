b2442af89ae02c415e3ec03b7d12282f867c1af8
Alexander Sosedkin <monk@unboiled.info>
2025-02-16 16:06:55 +0100

    flake.nix: switch to mainline nixos-hardware

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     nix-on-droid.inputs.nmd.follows = "nmd";
     nix-on-droid.inputs.nix-formatter-pack.follows = "nix-formatter-pack";
 
-    nixos-hardware.url = "github:t184256/nixos-hardware/onenetbook-4-patch-6.12";  # TODO: revert to mainline once #1341 is merged
+    nixos-hardware.url = "github:NixOS/nixos-hardware";
 
     impermanence.url = "github:nix-community/impermanence";
 
