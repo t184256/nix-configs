76df641c819d94135cb20db362a5a49042ff978c
Alexander Sosedkin <monk@unboiled.info>
2025-05-05 11:13:56 +0200

    overlays/alot: apply a compromise workaround to 1647

---
+++ overlays/alot/default.nix
@@ -0,0 +1,10 @@
+_: super:
+
+{
+  alot = super.alot.overrideAttrs ( oa: {
+    patches = (oa.patches or []) ++ [ ./workaround-1647.patch ];
+    disabledTests = (oa.disabledTests or []) ++ [
+      "tests/test_crypto.py::TestDecrypt::test_decrypt"
+    ];
+  });
+}
