77b526ac91bd91be53ebef9465ce5540d560737d
Alexander Sosedkin <monk@unboiled.info>
2025-08-03 23:28:15 +0200

    flake.lock, ...: update

--- nixos/common/tweaks.nix
+++ nixos/common/tweaks.nix
@@ -3,8 +3,6 @@
 {
   boot.tmp.useTmpfs = lib.mkDefault true;
   systemd.enableEmergencyMode = false;
-  systemd.extraConfig = ''
-    DefaultTimeoutStopSec=30s
-  '';
+  systemd.settings.Manager.DefaultTimeoutStopSec = "30s";
   services.journald.extraConfig = "SystemMaxUse=5%";
 }
