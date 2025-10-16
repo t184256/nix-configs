db0d73502ccdda05027ab3805251bd21918afb37
Alexander Sosedkin <monk@unboiled.info>
2025-10-16 11:26:11 +0200

    flake.lock, ...: update

--- overlays/alot/default.nix
+++ overlays/alot/default.nix
@@ -2,8 +2,7 @@ _: super:
 
 {
   alot =
-    (super.alot.override { python311 = super.python312; })
-    .overrideAttrs ( oa: {
+    super.alot.overrideAttrs ( oa: {
       patches = (oa.patches or []) ++ [ ./workaround-1647.patch ];
       disabledTests = (oa.disabledTests or []) ++ [
         "tests/test_crypto.py::TestDecrypt::test_decrypt"
