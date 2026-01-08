ea8f7e831704177cba864aad0be5ff52fbfeab40
Alexander Sosedkin <monk@unboiled.info>
2026-01-08 02:46:24 +0100

    hosts/watermelon/network: fix ipv6 route

--- hosts/watermelon/network.nix
+++ hosts/watermelon/network.nix
@@ -10,7 +10,7 @@ _:
         [ { address = "2602:ffd5:907:2::2"; prefixLength = 64; } ];
     };
     defaultGateway = "104.152.210.1";
-    defaultGateway6 = { address = "2602:ffd5:1:907:2::1"; interface = "enX0"; };
+    defaultGateway6 = { address = "2602:ffd5:907:2::1"; interface = "enX0"; };
     nameservers = [ "1.1.1.1" "2606:4700:4700::1111" ];
   };
 }
