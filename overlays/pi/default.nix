255e4638583bf276755be981746c3f80ec7847e8
Alexander Sosedkin <monk@unboiled.info>
2026-04-14 17:53:23 +0200

    overlays/pi: plumb thinkingBudgetTokens

--- overlays/pi/default.nix
+++ overlays/pi/default.nix
@@ -2,6 +2,7 @@ _: prev: {
   pi-coding-agent = prev.pi-coding-agent.overrideAttrs (oa: {
     patches = (oa.patches or []) ++ [
       ./compact-output.patch
+      ./thinking-budget-tokens.patch
     ];
   });
 }
