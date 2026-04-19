0e92edf362389f3a4de084c022f17c3ce2fee2ea
Alexander Sosedkin <monk@unboiled.info>
2026-04-19 22:56:50 +0200

    overlays/llama-cpp: update to 8849

--- overlays/llama-cpp/default.nix
+++ overlays/llama-cpp/default.nix
@@ -2,7 +2,7 @@ final: prev:
 # Doesn't work with non-default python version
 
 let
-  newerVer = "8744";
+  newerVer = "8849";
   overrides-fresh = old: {
     name = "llama-cpp-${newerVer}";
     version = newerVer;
@@ -10,14 +10,14 @@ let
       owner = "ggml-org";
       repo = "llama.cpp";
       tag = "b${newerVer}";
-      hash = "sha256-xLsJ8FjDzneyXlGXuGF5RV4xo3VHkMjNRGVPxS5Ihf0=";
+      hash = "sha256-SLiDrPcloriGIORvkuzUHCUpdr2Mt1Cm5NUQLZrXU8w=";
       leaveDotGit = true;
       postFetch = ''
         git -C "$out" rev-parse --short HEAD > $out/COMMIT
         find "$out" -name .git -print0 | xargs -0 rm -rf
       '';
     };
-    npmDepsHash = "sha256-eeftjKt0FuS0Dybez+Iz9VTVMA4/oQVh+3VoIqvhVMw=";
+    npmDepsHash = "sha256-RAFtsbBGBjteCt5yXhrmHL39rIDJMCFBETgzId2eRRk=";
     # add stub tools/server/public/index.html.gz to pacify upstream patchPhase
     prePatch = (old.prePatch or "") + ''
       mkdir -p tools/server/public
@@ -39,7 +39,6 @@ let
 
   llama-cpp-rocm-gfx1151 = llama-cpp-rocm.overrideAttrs (oa: {
     name = "llama-cpp-rocm-gfx1151-${newerVer}";
-    version = newerVer;
     patches = (oa.patches or []) ++ [
       # https://github.com/ggml-org/llama.cpp/pull/21344 slightly boosts PP,
       # and lowers TG, which I'm OK with for the slow-mo use case
