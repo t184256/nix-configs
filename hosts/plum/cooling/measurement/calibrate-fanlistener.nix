dd1c8e44fe6b0263b1fc731384328b3eb095026d
Alexander Sosedkin <monk@unboiled.info>
2026-05-20 18:12:48 +0200

    hosts/plum/cooling: start fanlistener later

--- hosts/plum/cooling/measurement/calibrate-fanlistener.nix
+++ hosts/plum/cooling/measurement/calibrate-fanlistener.nix
@@ -6,7 +6,11 @@
   systemd.services.calibrate-fanlistener = {
     description = "Fan noise measurement daemon";
     wantedBy = [ "multi-user.target" ];
+    after = [ "alsa-restore.service" ];
     serviceConfig = {
+      ExecStartPre = pkgs.writeShellScript "wait-for-audio-hw3" ''
+        until [ -e /dev/snd/pcmC3D0c ]; do sleep 1; done
+      '';
       ExecStart =
         "${pkgs.python3.withPackages (ps: [ ps.sounddevice ps.numpy ])}"
         + "/bin/python3 ${./calibrate-fanlistener.py} hw:3,0";
