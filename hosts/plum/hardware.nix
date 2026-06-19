950da55949a709e19714cfbafbff65f473bd507f
Alexander Sosedkin <monk@unboiled.info>
2026-06-19 20:15:36 +0200

    hosts/plum: initialize thunderbolt earlier to avoid hangs with eGPU

--- hosts/plum/hardware.nix
+++ hosts/plum/hardware.nix
@@ -12,6 +12,7 @@
       "usbhid"
       "xhci_pci"
     ];
+    initrd.kernelModules = [ "thunderbolt" ];  # gives thunderbolt time to init
     kernelModules = [ "kvm-amd" "nct6687" ];
     extraModulePackages = with config.boot.kernelPackages; [ nct6687d ];
     extraModprobeConfig = "options nct6687 msi_fan_brute_force=1";
