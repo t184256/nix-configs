01458a305dce85dcb42327f35c9ccb8384e470c0
Alexander Sosedkin <monk@unboiled.info>
2026-06-24 16:53:08 +0200

    nixos/common/tweaks: remove broken journald SystemMaxUse override

--- nixos/common/tweaks.nix
+++ nixos/common/tweaks.nix
@@ -4,5 +4,5 @@
   boot.tmp.useTmpfs = lib.mkDefault true;
   systemd.enableEmergencyMode = false;
   systemd.settings.Manager.DefaultTimeoutStopSec = "30s";
-  services.journald.extraConfig = "SystemMaxUse=5%";
+
 }
