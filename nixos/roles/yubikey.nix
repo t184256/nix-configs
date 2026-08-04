c79731fbf13928ddc2b2bcaef814e2d204fe59c0
Alexander Sosedkin <monk@unboiled.info>
2026-08-04 17:27:18 +0200

    nixos/roles/yubikey: add polkit rule

--- nixos/roles/yubikey.nix
+++ nixos/roles/yubikey.nix
@@ -21,6 +21,15 @@ in {
       services.pcscd.enable = true;
       services.udev.packages = [ pkgs.yubikey-personalization ];
       programs.ssh.startAgent = false;
+      security.polkit.extraConfig = ''
+        polkit.addRule(function(action, subject) {
+          if ((action.id == "org.debian.pcsc-lite.access_pcsc" ||
+               action.id == "org.debian.pcsc-lite.access_card") &&
+              subject.isInGroup("users")) {
+            return polkit.Result.YES;
+          }
+        });
+      '';
     })
   ];
 }
