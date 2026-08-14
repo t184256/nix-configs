af69866a82375d9134cd04f1db5e287e2a039b51
Alexander Sosedkin <monk@unboiled.info>
2026-08-14 10:14:53 +0200

    user/jailed-pi: add image input support for plum

--- user/jailed-pi.nix
+++ user/jailed-pi.nix
@@ -45,10 +45,12 @@ let
             cost = { input = 0.028; output = 0.28;
                      cacheRead = 0; cacheWrite = 0; }; }
           { id = "qwen3.6-27b-think"; reasoning = true;
+            input = [ "text" "image" ];
             contextWindow = 262144;
             cost = { input = 0.017; output = 0.59;
                      cacheRead = 0; cacheWrite = 0; }; }
           { id = "qwen3.6-27b-nothink";
+            input = [ "text" "image" ];
             contextWindow = 262144;
             cost = { input = 0.017; output = 0.59;
                      cacheRead = 0; cacheWrite = 0; }; }
@@ -65,11 +67,13 @@ let
         };
         models = [
           { id = "qwen3.6-27b-think"; reasoning = true;
+            input = [ "text" "image" ];
             contextWindow = 262144;
             cost = { input = 0; output = 0;
                      cacheRead = 0; cacheWrite = 0; };
             samplingParams = { return_progress = true; }; }
           { id = "qwen3.6-27b-nothink";
+            input = [ "text" "image" ];
             contextWindow = 262144;
             cost = { input = 0; output = 0;
                      cacheRead = 0; cacheWrite = 0; };
