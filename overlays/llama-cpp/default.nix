1e699053d1491b6f46bca5eaf82602d20a36460b
Alexander Sosedkin <monk@unboiled.info>
2026-08-27 13:14:18 +0200

    overlays/llama.cpp: update to v0.3.0 and refactor

--- overlays/llama-cpp/default.nix
+++ overlays/llama-cpp/default.nix
@@ -2,22 +2,31 @@ final: prev:
 # Doesn't work with non-default python version
 
 let
-  newerVer = "10313";
-  overrides-fresh = old: {
+  newerVer = "0.3.0";
+  # b-versions like 10408 compare higher that v-versions like 0.3.0,
+  # treat them as older
+  isFresh = v:
+    (prev.lib.strings.match "^[0-9]+$" v) == null &&
+    prev.lib.versionAtLeast v newerVer;
+  freshen = prevLlamaCpp:
+    if isFresh prevLlamaCpp.version
+    then prevLlamaCpp
+    else prevLlamaCpp.overrideAttrs overrides-fresh;
+  overrides-fresh = _: {
     name = "llama-cpp-${newerVer}";
     version = newerVer;
     src = prev.fetchFromGitHub {
       owner = "ggml-org";
       repo = "llama.cpp";
-      tag = "b${newerVer}";
-      hash = "sha256-tHqT6Fh4ZM8vBrA+hQjh+3kbq+6FL60rcdWCH1NT8eg=";
+      tag = "v${newerVer}";
+      hash = "sha256-eUHLOgWFy8N4vmrolnUxJYHPmtxmEmNGR4qL46mQs7A=";
       leaveDotGit = true;
       postFetch = ''
         git -C "$out" rev-parse --short HEAD > $out/COMMIT
         find "$out" -name .git -print0 | xargs -0 rm -rf
       '';
     };
-    npmDepsHash = "sha256-FHvd2bMvBc9EXrJEzu8EN78oUVSLcOKYCc0232V+L4A=";
+    npmDepsHash = "sha256-2Q7XhaLAArmviOLdQsNbYTfdyDE5pW9lR26cRHEVl9k=";
   };
   cuda-vulkan = (prev.llama-cpp.override {
     cudaSupport = true;
@@ -28,23 +37,10 @@ let
   });
 in
 rec {
-  llama-cpp =
-    if prev.lib.versionAtLeast prev.llama-cpp.version newerVer
-    then prev.llama-cpp
-    else prev.llama-cpp.overrideAttrs overrides-fresh;
-  llama-cpp-vulkan =
-    if prev.lib.versionAtLeast prev.llama-cpp-vulkan.version newerVer
-    then prev.llama-cpp-vulkan
-    else prev.llama-cpp-vulkan.overrideAttrs overrides-fresh;
-  llama-cpp-rocm =
-    if prev.lib.versionAtLeast prev.llama-cpp-rocm.version newerVer
-    then prev.llama-cpp-rocm
-    else prev.llama-cpp-rocm.overrideAttrs overrides-fresh;
-
-  llama-cpp-cuda-vulkan =
-    if prev.lib.versionAtLeast prev.llama-cpp-rocm.version newerVer
-    then cuda-vulkan
-    else cuda-vulkan.overrideAttrs overrides-fresh;
+  llama-cpp = freshen prev.llama-cpp;
+  llama-cpp-vulkan = freshen prev.llama-cpp-vulkan;
+  llama-cpp-rocm = freshen prev.llama-cpp-rocm;
+  llama-cpp-cuda-vulkan = freshen cuda-vulkan;
 
   llama-cpp-rocm-gfx1151 = (llama-cpp.override {
     rocmSupport = true;
