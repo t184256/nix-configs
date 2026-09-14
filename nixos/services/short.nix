2462697d86e3587c67939faf365a10cd0fefd20f
Alexander Sosedkin <monk@unboiled.info>
2026-09-14 19:07:57 +0200

    nixos/services/short: add csb-r

--- nixos/services/short.nix
+++ nixos/services/short.nix
@@ -16,6 +16,19 @@ let
     '';
     t14 = builtins.readFile ../../misc/inst/t14g5;
     intermezzo = builtins.readFile ../../misc/inst/intermezzo;
+    csb-r = ''
+      mkdir -p /tmp/csb; cd /tmp/csb
+      sudo dnf -y copr enable copr.devel.redhat.com/asosedki/multicsb
+      sudo dnf -y install --refresh multicsb
+      ls *.iso || multicsb download
+      [ -f csb.pristine.img ] || \
+        multicsb install $(ls -1 csb-fedora-*.iso | tail -n1) csb.pristine.img
+      [ -f csb.1st.img ] || \
+        multicsb first-boot --insecure-keep-keyfile --hardware-values \
+                 --postconfig /usr/libexec/multicsb/example-csb-postconfig \
+                 csb.pristine.img csb.1st.img
+      sudo multicsb $(date +%Y-%m-%d-%H-%M-%S) prime-pivot csb.1st.img
+    '';
   };
   short-scripts-dir = pkgs.linkFarm "short-scripts" (lib.mapAttrsToList
     (name: text: { inherit name; path = mkScript name text; }) scripts
