32a85998f094c765c98bf9f1931a36206823b9e3
Alexander Sosedkin <monk@unboiled.info>
2026-09-23 12:46:03 +0200

    nixos/services/nebula/nodes: refactor out

---
+++ nixos/services/nebula/nodes.nix
@@ -0,0 +1,36 @@
+{
+  loquat = {
+    int = "192.168.99.1"; ext = [ "loquat.unboiled.info" ]; routines = 3;
+  };
+  duckweed = { int = "192.168.99.2"; ext = [ "duckweed.unboiled.info" ]; };
+  bayroot = {
+    int = "192.168.99.3"; ext = [ "bayroot.dyn.unboiled.info" ]; v6 = true;
+  };
+  araceae = {
+    int = "192.168.99.4"; ext = [ "araceae.dyn.unboiled.info" ]; v6 = true;
+  };
+  quince = { int = "192.168.99.6"; routines = 2; };
+  cocoa = { int = "192.168.99.7"; routines = 10; };
+  lychee = { int = "192.168.99.8"; routines = 2; };
+  jujube = { int = "192.168.99.9"; routines = 2; };
+  sloe = {
+    int = "192.168.99.21"; ext = [ "sloe.unboiled.info" ]; routines = 3;
+  };
+  watermelon = {
+    int = "192.168.99.22"; ext = [ "watermelon.unboiled.info" ]; routines = 2;
+  };
+  olosapo = { int = "192.168.99.23"; ext = [ "olosapo.unboiled.info" ]; };
+  almond = { int = "192.168.99.31"; };
+  carambola = { int = "192.168.99.32"; };
+  pistachio = { int = "192.168.99.33"; };
+  candlenut = { int = "192.168.99.34"; };
+  # coconut is not here
+  nucifera = { int = "192.168.99.37"; };
+  wolfberry = { int = "192.168.99.38"; };
+  hackberry = { int = "192.168.99.39"; };
+  t14g5 = { int = "192.168.99.42"; };
+  intermezzo = { int = "192.168.99.43"; };
+  spondias = { int = "192.168.99.51"; routines = 2; };
+  grapefruit = { int = "192.168.99.52"; routines = 4; };
+  plum = { int = "192.168.99.53"; routines = 2; };
+}
